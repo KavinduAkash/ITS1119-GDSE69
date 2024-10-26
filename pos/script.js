cbb753a72f14d2147333b6255fde4cae073d1330
Kavindu Akash <bkkasamarasingha1220@gmail.com>
2024-10-26 13:46:38 +0530

    update sample pos

--- pos/script.js
+++ pos/script.js
@@ -1,6 +1,20 @@
 // customer array
 let customer_array = [];
 
+/*
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const sriLankanMobileRegex = /^(?:\+94|0)?7[0-9]{8}$/;
+*/
+
+const validateEmail = (email) => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+}
+
+const validateMobile = (mobile) => {
+    const sriLankanMobileRegex = /^(?:\+94|0)?7[0-9]{8}$/;
+    return sriLankanMobileRegex.test(mobile);
+}
 
 const loadCustomerTable = () => {
     $("#customerTableBody").empty();
@@ -11,49 +25,85 @@ const loadCustomerTable = () => {
     })
 }
 
-// Add Customer Button
-$("#customer_add_btn").on("click", function() {
-    let first_name = $('#firstName').val();
-    let last_name = $('#lastName').val();
-    let mobile = $('#mobile').val();
-    let email = $('#email').val();
-    let address = $('#address').val();
-
-    console.log("first_name: ", first_name);
-    console.log("last_name: ", last_name);
-    console.log("mobile: ", mobile);
-    console.log("email: ", email);
-    console.log("address: ", address);
-
-    let customer = {
-        id: customer_array.length + 1,
-        first_name: first_name,
-        last_name: last_name,
-        mobile: mobile,
-        email: email,
-        address: address
-    };
-
-    customer_array.push(customer);
-
-    // clean customer form
+const cleanCustomerForm  = () => {
     $('#firstName').val("");
     $('#lastName').val("")
     $('#email').val("");
     $('#mobile').val("");
     $('#address').val("");
+}
 
-    loadCustomerTable();
+// selected customer for update or delete
+let selected_customer_index = null;
+
+// Add Customer Button
+$("#customer_add_btn").on("click", function() {
+    let first_name = $('#firstName').val(); // empty
+    let last_name = $('#lastName').val(); // empty
+    let mobile = $('#mobile').val(); // empty, format
+    let email = $('#email').val(); // empty, format
+    let address = $('#address').val(); // empty
+
+    if(first_name.length===0) {
+        Swal.fire({
+            icon: "error",
+            title: "Invalid Input",
+            text: "Invalid First Name",
+        });
+    } else if(last_name.length===0) {
+        Swal.fire({
+            icon: "error",
+            title: "Invalid Input",
+            text: "Invalid Last Name",
+        });
+    } else if(!validateEmail(email)) {
+        Swal.fire({
+            icon: "error",
+            title: "Invalid Input",
+            text: "Invalid Email",
+        });
+    } else if(!validateMobile(mobile)) {
+        Swal.fire({
+            icon: "error",
+            title: "Invalid Input",
+            text: "Invalid Mobile",
+        });
+    } else if(address.length===0) {
+        Swal.fire({
+            icon: "error",
+            title: "Invalid Input",
+            text: "Invalid Address",
+        });
+    } else {
+        let customer = {
+            id: customer_array.length + 1,
+            first_name: first_name,
+            last_name: last_name,
+            mobile: mobile,
+            email: email,
+            address: address
+        };
+
+        customer_array.push(customer);
+
+        // clean customer form
+        cleanCustomerForm();
+
+        loadCustomerTable();
+    }
+
+
 
     // // create table row
     // let data = `<tr><td>${first_name}</td><td>${last_name}</td><td>${mobile}</td><td>${email}</td><td>${address}</td></tr>`
     // $("#customerTableBody").append(data);
 });
 
-
 $('#customerTableBody').on('click', 'tr', function () {
     // get tr index
     let index = $(this).index();
+
+    selected_customer_index = $(this).index();
 
     // get customer object by index
     let customer_obj = customer_array[index];
@@ -71,6 +121,83 @@ $('#customerTableBody').on('click', 'tr', function () {
     $('#email').val(email);
     $('#mobile').val(mobile);
     $('#address').val(address);
+});
+
+$('#customer_update_btn').on('click', function () {
+
+    let index = selected_customer_index;
+
+    let first_name = $('#firstName').val();
+    let last_name = $('#lastName').val();
+    let mobile = $('#mobile').val();
+    let email = $('#email').val();
+    let address = $('#address').val();
+
+    let customer = {
+        id: customer_array[index].id,
+        first_name: first_name,
+        last_name: last_name,
+        mobile: mobile,
+        email: email,
+        address: address
+    };
+    // update item
+    customer_array[selected_customer_index] = customer;
+
+    // clean customer form
+    cleanCustomerForm();
+
+    // reload the table
+    loadCustomerTable();
+});
+
+$("#customer_delete_btn").on('click', function () {
+
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: "btn btn-success",
+            cancelButton: "btn btn-danger"
+        },
+        buttonsStyling: false
+    });
+    swalWithBootstrapButtons.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Yes, delete it!",
+        cancelButtonText: "No, cancel!",
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+
+            // ==========================================================
+            customer_array.splice(selected_customer_index, 1);
+
+            // clean customer form
+            cleanCustomerForm();
+
+            // reload the table
+            loadCustomerTable();
+            // ==========================================================
+
+            swalWithBootstrapButtons.fire({
+                title: "Deleted!",
+                text: "Your customer has been deleted.",
+                icon: "success"
+            });
+        } else if (
+            /* Read more about handling dismissals below */
+            result.dismiss === Swal.DismissReason.cancel
+        ) {
+            swalWithBootstrapButtons.fire({
+                title: "Cancelled",
+                text: "Your imaginary file is safe :)",
+                icon: "error"
+            });
+        }
+    });
+
 });
 
 
